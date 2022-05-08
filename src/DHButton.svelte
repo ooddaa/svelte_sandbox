3b83f8f6162109b5a4490952ce09802539d95299
oda <ooddaa@gmail.com>
2022-05-08 23:35:06 +0100

    DHButton - you have clicked way many times, buddy

--- src/DHButton.svelte
+++ src/DHButton.svelte
@@ -12,78 +12,82 @@
 </script>
 
 <main class="center">
-  <div class="dh-button center" on:click={handleClick}>
-    <svg
-      class="logo"
-      viewBox="0 0 11 13"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g clip-path="url(#clip0_7_26)">
-        <path
-          d="M0 1C0 0.447715 0.447715 0 1 0H2V9H0V1Z"
-          fill="#131212"
-          fill-opacity="0.93"
-        />
-        <rect
-          width="2"
-          height="8"
-          transform="translate(3 5)"
-          fill="#131212"
-          fill-opacity="0.93"
-        />
-        <path
-          d="M6 0H10C10.5523 0 11 0.447715 11 1V2H6V0Z"
-          fill="#131212"
-          fill-opacity="0.93"
-        />
-        <rect
-          width="2"
-          height="3"
-          transform="translate(9 2)"
-          fill="#131212"
-          fill-opacity="0.93"
-        />
-        <rect
-          width="2"
-          height="10"
-          transform="translate(6 3)"
-          fill="#131212"
-          fill-opacity="0.93"
-        />
-        <path
-          d="M11 6H9V13H10C10.5523 13 11 12.5523 11 12V6Z"
-          fill="#131212"
-          fill-opacity="0.93"
-        />
-        <path
-          d="M0 10H2V13H1C0.447715 13 0 12.5523 0 12V10Z"
-          fill="#131212"
-          fill-opacity="0.93"
-        />
-        <rect
-          width="2"
-          height="4"
-          transform="translate(3)"
-          fill="#131212"
-          fill-opacity="0.93"
-        />
-      </g>
-      <defs>
-        <clipPath id="clip0_7_26">
+  <div class="container center">
+    <div class="dh-button center" on:click={handleClick}>
+      <svg
+        class="logo"
+        viewBox="0 0 11 13"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clip-path="url(#clip0_7_26)">
           <path
-            d="M0 1C0 0.447715 0.447715 0 1 0H10C10.5523 0 11 0.447715 11 1V12C11 12.5523 10.5523 13 10 13H1C0.447715 13 0 12.5523 0 12V1Z"
-            fill="white"
+            d="M0 1C0 0.447715 0.447715 0 1 0H2V9H0V1Z"
+            fill="#131212"
+            fill-opacity="0.93"
           />
-        </clipPath>
-      </defs>
-    </svg>
+          <rect
+            width="2"
+            height="8"
+            transform="translate(3 5)"
+            fill="#131212"
+            fill-opacity="0.93"
+          />
+          <path
+            d="M6 0H10C10.5523 0 11 0.447715 11 1V2H6V0Z"
+            fill="#131212"
+            fill-opacity="0.93"
+          />
+          <rect
+            width="2"
+            height="3"
+            transform="translate(9 2)"
+            fill="#131212"
+            fill-opacity="0.93"
+          />
+          <rect
+            width="2"
+            height="10"
+            transform="translate(6 3)"
+            fill="#131212"
+            fill-opacity="0.93"
+          />
+          <path
+            d="M11 6H9V13H10C10.5523 13 11 12.5523 11 12V6Z"
+            fill="#131212"
+            fill-opacity="0.93"
+          />
+          <path
+            d="M0 10H2V13H1C0.447715 13 0 12.5523 0 12V10Z"
+            fill="#131212"
+            fill-opacity="0.93"
+          />
+          <rect
+            width="2"
+            height="4"
+            transform="translate(3)"
+            fill="#131212"
+            fill-opacity="0.93"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_7_26">
+            <path
+              d="M0 1C0 0.447715 0.447715 0 1 0H10C10.5523 0 11 0.447715 11 1V12C11 12.5523 10.5523 13 10 13H1C0.447715 13 0 12.5523 0 12V1Z"
+              fill="white"
+            />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+    {#if count > 5}
+      <div class="clicked-enough center">you have clicked {count} times
+        <button class="mt-10" on:click={() => count = 0}>Remove</button>
+      </div>
+    {:else}
+      <div class="clicked-enough invisible" />
+    {/if}
   </div>
-  {#if count > 5}
-    <div class="clicked-enough">you have clicked {count} times</div>
-  {:else}
-    <div class="clicked-enough invisible" />
-  {/if}
 </main>
 
 <style>
@@ -101,10 +105,17 @@
     flex-direction: column;
   }
 
+  .container {
+    flex: 0 0 200px;
+    width: 300px;
+  }
+
   .dh-button {
     width: 110px;
     height: 130px;
     cursor: pointer;
+    align-self: flex-start;
+    align-self: center;
   }
 
   .dh-button:active {
@@ -118,5 +129,10 @@
 
   .clicked-enough {
     height: 25px;
+    align-self: center;
+  }
+
+  .mt-10 {
+    margin-top: 10px;
   }
 </style>
